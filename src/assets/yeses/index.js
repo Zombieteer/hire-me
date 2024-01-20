262b9b9d0ebcca66cbadbb9c22905061ee4f7d97
zombieteer <aryan.nigam1996@gmail.com>
2024-01-21 02:49:14 +0530

    bundle size reduced

--- src/assets/yeses/index.js
+++ src/assets/yeses/index.js
@@ -1,7 +1,5 @@
 import chipiCat from "./chipi-cat.mp4";
 import happyCat from "./happy-cat.mp4";
-import oldMan from "./old-man.png";
-import spongeBob from "./spongebob.jpeg";
 
 const yesList = [chipiCat, happyCat];
 export default yesList;
